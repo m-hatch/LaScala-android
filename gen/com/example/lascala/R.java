3e4b3dbd9ee2056c72002f0aaa70ede6e602f128
m-hatch <montehatch@gmail.com>
2014-09-16 00:17:18 -0400

    rebuild as LaScala

--- gen/com/example/lascala/R.java
+++ gen/com/example/lascala/R.java
@@ -26,8 +26,9 @@ public final class R {
         public static final int buttonshape=0x7f020001;
         public static final int clams=0x7f020002;
         public static final int ic_launcher=0x7f020003;
-        public static final int logo=0x7f020004;
-        public static final int tiramisu=0x7f020005;
+        public static final int ic_launcher_dinner=0x7f020004;
+        public static final int logo=0x7f020005;
+        public static final int tiramisu=0x7f020006;
     }
     public static final class id {
         public static final int action_settings=0x7f080004;
